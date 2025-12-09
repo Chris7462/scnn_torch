8fe8b175022f4e95c8beb482142df1c6d5dc8c5a
chris7462 <chris7462@gmail.com>
2025-12-09 15:02:45 -0500

    add type hint for tensorboard

--- utils/tensorboard.py
+++ utils/tensorboard.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import numpy as np
 from torch.utils.tensorboard import SummaryWriter
 
@@ -5,14 +7,14 @@ from torch.utils.tensorboard import SummaryWriter
 class TensorBoard:
     """TensorBoard logging wrapper."""
 
-    def __init__(self, log_dir):
+    def __init__(self, log_dir: str | Path) -> None:
         """
         Args:
             log_dir: Directory to save TensorBoard logs
         """
         self.writer = SummaryWriter(log_dir)
 
-    def scalar_summary(self, tag, value, step):
+    def scalar_summary(self, tag: str, value: float, step: int) -> None:
         """
         Log a scalar variable.
 
@@ -23,7 +25,7 @@ class TensorBoard:
         """
         self.writer.add_scalar(tag, value, step)
 
-    def image_summary(self, tag, images, step):
+    def image_summary(self, tag: str, images: list[np.ndarray], step: int) -> None:
         """
         Log a list of images.
 
@@ -38,21 +40,10 @@ class TensorBoard:
                 img = np.transpose(img, (2, 0, 1))
             self.writer.add_image(f'{tag}/{i}', img, step)
 
-    def histogram_summary(self, tag, values, step):
-        """
-        Log a histogram of values.
-
-        Args:
-            tag: Name of the histogram
-            values: Values to log
-            step: Training step or epoch
-        """
-        self.writer.add_histogram(tag, values, step)
-
-    def flush(self):
+    def flush(self) -> None:
         """Flush the writer."""
         self.writer.flush()
 
-    def close(self):
+    def close(self) -> None:
         """Close the writer."""
         self.writer.close()

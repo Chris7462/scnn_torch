c45b9cc52a0ffda696e8f987b448c0e62e6cd8f9
chris7462 <chris7462@gmail.com>
2025-12-16 22:07:32 -0500

    fix the transform

--- datasets/transforms.py
+++ datasets/transforms.py
@@ -1,3 +1,5 @@
+import cv2
+
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
@@ -15,7 +17,7 @@ def get_train_transforms(
         resize_shape: Target size as (height, width)
         mean: Sequence of means for each channel
         std: Sequence of standard deviations for each channel
-        rotation: Maximum rotation angle in degrees (samples from [-rotation/2, rotation/2])
+        rotation: Rotation theta in degrees (samples from [-theta/2, theta/2])
 
     Returns:
         Albumentations Compose transform
@@ -24,8 +26,12 @@ def get_train_transforms(
     # then ToTensorV2 converts to tensor. This is different from pytorch's Normalize
     # and ToTensor.
     return A.Compose([
-        A.Resize(height=resize_shape[0], width=resize_shape[1]),
-        A.Rotate(limit=rotation, border_mode=0, p=0.5),
+        A.Resize(
+            height=resize_shape[0],
+            width=resize_shape[1],
+            interpolation=cv2.INTER_CUBIC
+        ),
+        A.Rotate(limit=rotation / 2, border_mode=0, p=1.0),
         A.Normalize(mean=mean, std=std),
         ToTensorV2(),
     ])
@@ -51,7 +57,11 @@ def get_val_transforms(
     # then ToTensorV2 converts to tensor. This is different from pytorch's Normalize
     # and ToTensor.
     return A.Compose([
-        A.Resize(height=resize_shape[0], width=resize_shape[1]),
+        A.Resize(
+            height=resize_shape[0],
+            width=resize_shape[1],
+            interpolation=cv2.INTER_CUBIC
+        ),
         A.Normalize(mean=mean, std=std),
         ToTensorV2(),
     ])

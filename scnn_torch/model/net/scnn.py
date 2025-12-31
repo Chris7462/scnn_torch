e747f2023d70560c39915d2557ef451c654f63b1
chris7462 <chris7462@gmail.com>
2025-12-31 08:59:48 -0500

    experiment conv 5x3 dilation 2

--- scnn_torch/model/net/scnn.py
+++ scnn_torch/model/net/scnn.py
@@ -65,7 +65,7 @@ class SCNN(nn.Module):
         self.message_passing = MessagePassing(channels=128, kernel_size=ms_ks)
         self.seg_neck = SegNeck(in_channels=128, out_channels=5)
         self.seg_head = SegHead(upsample_scale=8)
-        self.exist_head = ExistHead(in_channels=5)
+        self.exist_head = ExistHead(in_channels=5, mid_channels=8, num_lanes=4)
 
         self._initialize_weights(pretrained)
 
